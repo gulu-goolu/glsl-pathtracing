aa877d2bffa222c31c2808168695f26e73b026cf
murmur wheel <murmur.wheel@gmail.com>
2020-03-22 04:11:00 +0800

    shader compile

---
+++ shaders/trace.comp.glsl
@@ -0,0 +1,5 @@
+#version 450 core
+
+layout(set = 0, binding = 0) uniform sampler2D tex;
+
+void main() {}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+void checkShaderExists() {
+  FILE *fp;
+  if (fopen_s(&fp, "trace.comp.spv", "rb") != 0 || !fp) {
+    printf("resources not exists\n");
+  } else {
+    printf("found shader\n");
+  }
+
+  if (fp) {
+    fclose(fp);
+  }
 }
+
+int main() { checkShaderExists(); }
