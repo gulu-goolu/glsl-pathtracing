c476375c2575286dc4eeda41ecb804336c198d1d
murmur wheel <murmur.wheel@gmail.com>
2020-03-22 04:11:53 +0800

    clang format

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 void checkShaderExists() {
-  FILE *fp;
-  if (fopen_s(&fp, "trace.comp.spv", "rb") != 0 || !fp) {
-    printf("resources not exists\n");
-  } else {
-    printf("found shader\n");
-  }
+    FILE *fp;
+    if (fopen_s(&fp, "trace.comp.spv", "rb") != 0 || !fp) {
+        printf("resources not exists\n");
+    } else {
+        printf("found shader\n");
+    }
 
-  if (fp) {
-    fclose(fp);
-  }
+    if (fp) {
+        fclose(fp);
+    }
 }
 
-int main() { checkShaderExists(); }
+int main() {
+    checkShaderExists();
+}
