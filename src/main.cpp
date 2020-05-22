1e0bc97b05edd6580831af5c24a2664dfef27194
murmur wheel <murmur.wheel@gmail.com>
2020-05-23 01:42:40 +0800

    vulkan support

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
+#include <vulkan/vulkan.h>
 #include <cstdio>
+
 int main() {
-    printf("hello, raytracing\n");
+    VkInstance instance = VK_NULL_HANDLE;
+    VkInstanceCreateInfo instance_create_info = {};
+    instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    if (vkCreateInstance(&instance_create_info, nullptr, &instance) == VK_SUCCESS) {
+        printf("hello, vulkan\n");
+        vkDestroyInstance(instance, nullptr);
+    }
 }
