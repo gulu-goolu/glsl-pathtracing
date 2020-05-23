da46df08743362c82b70a3f760f3c5946ae81573
murmur wheel <murmur.wheel@gmail.com>
2020-05-23 16:41:52 +0800

    glfw

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,20 @@
 // Created by murmur.wheel@gmail.com on 2020/5/23.
 //
 
-#include "util.h"
+#include "app.h"
+#include <GLFW/glfw3.h>
 
 int main() {
-    Blob shader_frag_blob;
-    if (read_file("res/compile.frag.spv", shader_frag_blob)) {
-        printf("load shader success!!\n");
+    if (!glfwInit()) {
+        return 1;
     }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    auto window = glfwCreateWindow(1920, 1080, "window", nullptr, nullptr);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 }
