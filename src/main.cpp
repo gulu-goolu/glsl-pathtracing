38ea54dba8fd024cee18243ef2282340e5d1a69c
caohongjian <caohongjian@bigo.sg>
2020-06-12 22:44:03 +0800

    test vulkan

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,25 @@
 
 #include "app.h"
 
+void test_vulkan() {
+  VkInstance instance;
+  VkInstanceCreateInfo create_info = {};
+  create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  if (vkCreateInstance(&create_info, nullptr, &instance) == VK_SUCCESS) {
+    printf("init success!\n");
+  } else {
+    printf("init failed!\n");
+  }
+
+  vkDestroyInstance(instance, nullptr);
+}
+
 int main() {
   if (!glfwInit()) {
     return 1;
   }
+
+  test_vulkan();
 
   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
   glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
